12dcb9ac520df956386f2a576f175293d089cd60
AswathiVV <aswathivv916@gmail.com>
2025-01-27 13:33:49 +0530

    Portfolio

--- My_Portfolio/home.py
+++ My_Portfolio/home.py
@@ -1,19 +1,31 @@
-# from flask import Flask,render_template
-
-# app= Flask(__name__)
-
-# @app.route('/')
-# def fun1():
-#     return render_template("index.html")
-
-# app.run()
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'aswathivv916@gmail.com'
+app.config['MAIL_PASSWORD'] = 'jtno caxc bmgx myyn'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+    
+@app.route('/send_mail',methods=['POST'])
+def send_message():
+    name=request.form['name']
+    email=request.form['email']
+    message=request.form['message']
+    msg = Message(subject='Hello', sender=email, recipients=['aswathivv916@gmail.com'])
+    msg.body = f"From: {name}\n {email}\n\nMessage:\n{message}"
+    mail.send(msg)
+    return render_template('index.html', success=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+    
+app.run()    
